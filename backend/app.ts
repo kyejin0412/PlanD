d485e4b9cbb02af644792ff2c8ef0ca615d5a888
kyejin0412@kookmin.ac.kr <kyejin0412@kookmin.ac.kr>
2024-05-26 22:35:26 +0900

    express.js 초기설정

---
+++ backend/app.ts
@@ -0,0 +1,56 @@
+import createError from "http-errors";
+import express, { NextFunction, Request, Response } from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+
+import indexRouter from "./routes/index";
+import usersRouter from "./routes/users";
+
+const app = express();
+
+app.listen(8080, () => {
+  console.log("server on");
+});
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use("/", express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(createError(404));
+});
+
+// error handler
+app.use(
+  (
+    err: createError.HttpError,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+  }
+);
+
+export default app;
